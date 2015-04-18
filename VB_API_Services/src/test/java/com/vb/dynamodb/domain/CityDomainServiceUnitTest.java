b0eda8ac375ad06268d0d30c8d8f32be2b795043
teamwork523 <teamworkucla@hotmail.com>
2015-04-18 09:18:43 -0700

    Todo write integration test instead of unit test for domain layer

--- VB_API_Services/src/test/java/com/vb/dynamodb/domain/CityDomainServiceUnitTest.java
+++ VB_API_Services/src/test/java/com/vb/dynamodb/domain/CityDomainServiceUnitTest.java
@@ -22,6 +22,7 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedList;
 import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedQueryList;
 import com.vb.dynamodb.connector.DynamoDBConnector;
 import com.vb.dynamodb.model.CityItem;
@@ -31,6 +32,9 @@ import com.vb.dynamodb.model.CityItem;
  * 
  * @author Haokun Luo
  */
+
+// TODO: make a lifecycle integration test here
+
 //@RunWith(PowerMockRunner.class)
 //@PrepareForTest({DynamoDBConnector.class})
 public class CityDomainServiceUnitTest extends EasyMockSupport {
@@ -42,7 +46,7 @@ public class CityDomainServiceUnitTest extends EasyMockSupport {
 	public void setUp() throws Exception {
 		PowerMock.mockStatic(DynamoDBMapper.class);
 		m_cityDomainService = new CityDomainServiceImpl();
-		DynamoDBConnector.dynamoDBMapper = createMock(DynamoDBMapper.class);
+		//DynamoDBConnector.dynamoDBMapper = createMock(DynamoDBMapper.class);
 	}
 	
 	////////////////////////////////////////////////
@@ -58,7 +62,7 @@ public class CityDomainServiceUnitTest extends EasyMockSupport {
 			   testStateName = "Test State",
 			   testCountryName = "Test Country";
 		CityItem inputCity = new CityItem(testCityName, testStateName, testCountryName);
-		List<CityItem> cityList = new ArrayList<CityItem>();
+		PaginatedQueryList<CityItem> cityList = null;
 		CityItem cityKey = new CityItem();
 		cityKey.setCityName("test_city");
 		DynamoDBQueryExpression<CityItem> queryExpression = 
@@ -66,9 +70,9 @@ public class CityDomainServiceUnitTest extends EasyMockSupport {
 			        .withHashKeyValues(cityKey);
 	
 		// Setup Mock
-		expect(m_cityDomainService.retrieveCitiesByName("test_city")).andReturn(null);
-		DynamoDBConnector.dynamoDBMapper.save(isA(CityItem.class));
-		expectLastCall();
+		//expect(DynamoDBConnector.dynamoDBMapper.query(CityItem.class, queryExpression)).andReturn(cityList);
+		//DynamoDBConnector.dynamoDBMapper.save(isA(CityItem.class));
+		//expectLastCall();
 		
 		replayAll();
 		
