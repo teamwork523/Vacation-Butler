45678f7a20f9097e10343387c1c16103e60660e9
teamwork523 <teamworkucla@hotmail.com>
2015-05-01 10:40:50 -0700

    Update log file path

--- VB_API_Services/StartApp.sh
+++ VB_API_Services/StartApp.sh
@@ -3,4 +3,4 @@
 export JVM_OPTIONS="-Xmx1g -XX:MaxPermSize=256m -agentlib:jdwp=transport=dt_socket,server=y,address=8001,suspend=n"
 # Search for the "FAT" jar, and spin off the Jetty server locally
 #java $JVM_OPTIONS -jar $(find . -iname vb-api-services*.jar -type f) server 
-java $JVM_OPTIONS -jar $(find . -iname vb-api-services*.jar -type f) server src/main/resources/vb-api-services.yml 
+java $JVM_OPTIONS -jar $(find . -iname vb-api-services*.jar -type f) server src/test/resources/vb-api-services-local.yml 
