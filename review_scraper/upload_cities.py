9d54c33deb1c2d04751e3c6fe2c62743bd56fbec
Yijie Wang <wang21533@gmail.com>
2015-06-07 14:34:07 -0700

    bug fix

--- review_scraper/upload_cities.py
+++ review_scraper/upload_cities.py
@@ -1,5 +1,6 @@
 import json
 import urllib2
+import time
 
 url = "http://dev-env-xwauaaztim.elasticbeanstalk.com/api/city/createcity"
 
@@ -10,9 +11,9 @@ with open('list_of_cities.txt', 'r') as city_file:
     for line in city_file:
         city_dict = {}
         city_split_list = line.split(';',2)
-        city_dict['City Name'] = city_split_list[0]
-        city_dict['State Name'] = city_split_list[1]
-        city_dict['Country Name'] = city_split_list[2]
+        city_dict['City Name'] = city_split_list[2].strip()
+        city_dict['State Name'] = city_split_list[1].strip()
+        city_dict['Country Name'] = city_split_list[0].strip()
         try:
             response = urllib2.urlopen(req, json.dumps(city_dict)).read()
         except urllib2.HTTPError, e:
@@ -21,3 +22,4 @@ with open('list_of_cities.txt', 'r') as city_file:
             else:
                 raise
         print response
+        time.sleep(1)
