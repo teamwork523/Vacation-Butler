99ce7357bf802d0587a958b6ffe061d76edb51e1
Yijie Wang <wang21533@gmail.com>
2015-06-21 12:56:27 -0700

    cleaned up state names

--- review_scraper/upload_cities.py
+++ review_scraper/upload_cities.py
@@ -1,6 +1,7 @@
 import json
 import urllib2
 import time
+import re
 
 url = "http://dev-env-xwauaaztim.elasticbeanstalk.com/api/city/createcity"
 
@@ -9,9 +10,12 @@ with open('list_of_cities.txt', 'r') as city_file:
         city_dict = {}
         city_split_list = line.split(';',2)
         city_dict['City Name'] = city_split_list[2].strip()
-        city_dict['State Name'] = city_split_list[1].strip()
+        state_name = city_split_list[1].strip()
+        state_name = re.sub(r'\([\w]+\)','',state_name) #remove parentheses
+        city_dict['State Name'] = state_name.strip()
         city_dict['Country Name'] = city_split_list[0].strip()
         try:
+            print city_dict['City Name'], city_dict['State Name'], city_dict['Country Name']
             req = urllib2.Request(url)
             req.add_header('Content-Type', 'application/json')
             response = urllib2.urlopen(req, json.dumps(city_dict)).read()
